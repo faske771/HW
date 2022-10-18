8e9cc569f06ef63763d243216eea0d3a541f3d8a
Artr <example.com>
2022-10-18 18:49:53 +0300

    hw3

---
+++ hw3/ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string array = System.Console.ReadLine();
+int n = array.Length;
+int n2 = n;
+int b = 0;
+for (int i = 0; i <= n/2; i++)
+{
+    if (n == 5) { 
+    if(array[i] != array[n2-1])
+    {
+        break;
+    }
+    else{
+        n2 = n2 - 1;
+        b = 1;
+    }
+    }
+    else {
+       Console.WriteLine("Введите пятизначное число");
+       break;
+    }
+} 
+if (b==1) System.Console.WriteLine($"Число {array} является палиндромом");
+else System.Console.WriteLine($"Число {array} не является палиндромом");
+
---
+++ hw3/ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Введите N");
+int N = int.Parse(Console.ReadLine());
+/*int[] array = new int[N];
+for (int i=1;i<N;i++)
+{
+    array[i]=i+1;
+}*/
+int[] newarray = new int[N];
+for (int j=0;j<N;j++)
+{
+    double cube = Math.Pow(j+1, 3);
+    int intcube = (int)cube; 
+    newarray[j] = intcube;
+
+}
+var arr = string.Join(" ", newarray);
+Console.WriteLine(arr);
